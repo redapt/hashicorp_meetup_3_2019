e5566fe5d7e3553614f23dd66ba2644e985aa5d2
Ian Cornett <icornett@redapt.com>
2019-02-22 18:00:03 -0800

    first pass at AWS code

---
+++ modules/aws/data.tf
@@ -0,0 +1,26 @@
+# Define DataSources
+data "aws_vpc" "selected" {
+  id = "${aws_vpc.vpc.id}"
+}
+
+data "aws_availability_zones" "all" {}
+
+data "aws_region" "current" {
+  current = true
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
---
+++ modules/aws/main.tf
@@ -0,0 +1,54 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.12.0.0/16"
+  tags      = "${var.tags}"
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  cidr_block = "${cidrsubnet(aws_vpc.vpc.cidr_block, 8, 12)}"
+  tags = "${var.tags}"
+}
+
+# Allow Internet connectivity
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${data.aws_vpc.selected.id}"
+
+  tags {
+    Name = "demo-inetgw-${var.environmentName}"
+  }
+}
+
+# Add FE subnets to default route table
+resource "aws_route_table_association" "rta" {
+  count          = "${length(data.aws_availability_zones.all.names)}"
+  subnet_id      = "${aws_subnet.subnet.id}"
+  route_table_id = "${aws_vpc.vpc.default_route_table_id}"
+}
+
+# Add route to internet gateway for outbound
+resource "aws_route" "allow_outbound" {
+  route_table_id         = "${aws_vpc.vpc.default_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.gw.id}"
+}
+
+# Import SSH KeyPair
+resource "aws_key_pair" "keypair" {
+  key_name = "${var.key_name}"
+  public_key = "${file(var.public_key_path)}"
+}
+
+# Create VM host for Docker
+resource "aws_instance" "docker" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+  key_name = "${var.key_name}"
+  tags = "${var.tags}"
+}
+
+# Create Public IP
+resource "aws_eip" "public" {
+  instance = "${aws_instance.docker.id}"
+  vpc = true
+  tags = "${var.tags}"
+}
