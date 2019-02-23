de04e00b25e8d1d7585acdb28e28181200290d3b
Ian Cornett <icornett@redapt.com>
2019-02-22 21:46:14 -0800

    add azure module

--- main.tf
+++ main.tf
@@ -3,4 +3,14 @@ module "aws" {
   key_name        = "hashicorp-meetup-key"
   public_key_path = "${var.public_key_path}"
   script_path     = "${var.userdata_path}"
+  tags            = "${var.tags}"
+}
+
+module "azure" {
+  source          = "./modules/azure"
+  location        = "westus2"
+  tags            = "${var.tags}"
+  script_path     = "${var.userdata_path}"
+  public_key_path = "${var.public_key_path}"
+  name            = "hashicorp-meetup"
 }
--- output.tf
+++ output.tf
@@ -1,3 +1,8 @@
-output "public_ip" {
+output "aws_public_ip" {
   value = "${module.aws.public_ip}"
 }
+
+output "azure_public_ip" {
+  value = "${module.azure.public_ip}"
+}
+
