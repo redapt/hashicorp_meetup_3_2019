83f2356f9f9ca8badc195279c98332088fbef25d
Ian Cornett <icornett@redapt.com>
2019-03-01 21:58:24 -0800

    add rg name and use_msi flag

--- backend.tf
+++ backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "azurerm" {
+    resource_group_name  = "hashicorp-meetup-backend-rg"
     storage_account_name = "redapthashicorpmeetup"
     container_name       = "terraform-storage"
     key                  = "hashicorp-platform.tfstate"
+    use_msi              = true
   }
 }
