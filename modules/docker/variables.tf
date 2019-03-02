f4288d0c0d7019940a6b17d71113b3dc1f3027bf
Ian Cornett <icornett@redapt.com>
2019-03-01 23:02:44 -0800

    add provider and variables

--- modules/docker/variables.tf
+++ modules/docker/variables.tf
@@ -1,1 +1,11 @@
+variable "frontend_ip" {
+  description = "The IP for the Frontend Container"
+}
 
+variable "database_ip" {
+  description = "The IP of the Database Container"
+}
+
+variable "domain_name" {
+  description = "The domain name to proxy in NGINX"
+}
