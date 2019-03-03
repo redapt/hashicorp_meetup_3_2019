b7194bfa0fb3b3e7f5c45fb47506511a186853f5
Ian Cornett <icornett@redapt.com>
2019-03-02 22:55:46 -0800

    add docker registry creds

--- app_config/provider.tf
+++ app_config/provider.tf
@@ -1,6 +1,12 @@
 provider "docker" {
   alias = "fe"
   host = "tcp://${var.frontend_ip}:2375"
+
+  registry_auth {
+    address = "registry.hub.docker.com"
+    username = "${var.docker_username}"
+    password = "${var.docker_password}"
+  }
 }
 
 provider "docker" {
