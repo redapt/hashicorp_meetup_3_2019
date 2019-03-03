84be855fe08720d8d9e3006e7c8ee14bdc43670f
Ian Cornett <icornett@redapt.com>
2019-03-02 19:48:19 -0800

    set port to unencrypted 2375

--- app_config/provider.tf
+++ app_config/provider.tf
@@ -1,11 +1,11 @@
 provider "docker" {
   alias = "fe"
-  host = "tcp://${var.frontend_ip}:2376"
+  host = "tcp://${var.frontend_ip}:2375"
 }
 
 provider "docker" {
   alias = "db"
-  host = "tcp://${var.backend_ip}:2376"
+  host = "tcp://${var.backend_ip}:2375"
 
   registry_auth {
     address = "registry.hub.docker.com"
