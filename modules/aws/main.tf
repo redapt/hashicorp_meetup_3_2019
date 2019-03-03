73601de6a102bb292b411b25016ff1176ad03cb2
Ian Cornett <icornett@redapt.com>
2019-03-02 21:52:43 -0800

    add az to subnet

--- modules/aws/main.tf
+++ modules/aws/main.tf
@@ -65,6 +65,7 @@ resource "aws_vpc" "vpc" {
 resource "aws_subnet" "subnet" {
   vpc_id     = "${aws_vpc.vpc.id}"
   cidr_block = "${cidrsubnet(aws_vpc.vpc.cidr_block, 8, 1)}"
+  availability_zone = "us-west-2a"
 
   tags {
     Project  = "Hashicorp Meetup 3/6/2019"
