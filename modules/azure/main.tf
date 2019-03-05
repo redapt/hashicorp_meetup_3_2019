847bd9122922f2b0450ea443100ca9592c086ea7
Ian Cornett <icornett@redapt.com>
2019-03-04 21:12:05 -0800

    set ip to static

--- modules/azure/main.tf
+++ modules/azure/main.tf
@@ -42,7 +42,7 @@ resource "azurerm_public_ip" "ip" {
   name                = "${local.name_prefix}-ip"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   location            = "${var.location}"
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 resource "azurerm_virtual_machine" "vm" {
